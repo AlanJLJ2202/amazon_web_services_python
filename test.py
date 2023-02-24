3fbe32ee8dd1b0263dfcbf52cb2c6a0c29d7d96b
AlanJLJ2202 <alanjlj2202@gmail.com>
2023-02-24 07:54:08 -0600

    Structured code

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
+import boto3
 import pandas as pd
+from io import StringIO, BytesIO
+from datetime import datetime, timedelta
 
-print('Hola Mundo')
 
-pd.array([1, 2, 3])
+# Adapter Layer
+
+def read_csv_to_df(bucket, objects):
+
+    # Obtener el objeto por posición en el bucket objects
+    csv_obj_init = bucket.Object(key=objects[34].key).get().get('Body').read().decode('utf-8')
+    data = StringIO(csv_obj_init)
+    df_init = pd.read_csv(data, delimiter=',')
+
+    # Se inicializa el nombre de las columnas extrayendo los nombres de df_init
+    df_all = pd.DataFrame(columns=df_init.columns)
+    return df_all
+
+def write_df_to_s3():
+    pass
+
+def return_objects():
+    pass
